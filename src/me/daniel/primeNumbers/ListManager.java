25cdac0c5443e93aab7a643adf8c98c2ef749542
Daniel Kowalski <danielk.biznes@gmail.com>
2018-08-24 00:26:21 +0200

    Add methods to control cursor in list manager

---
+++ src/me/daniel/primeNumbers/ListManager.java
@@ -0,0 +1,54 @@
+package me.daniel.primeNumbers;
+
+import java.math.BigInteger;
+
+import java.util.ArrayList;
+
+public class ListManager {
+    private ArrayList<BigInteger> numbersList;
+    private int cursor;
+    
+    public ListManager() {
+        numbersList = new ArrayList<BigInteger>();
+    }
+    
+    /*
+     * Working on list
+     */
+    
+    public void addNumber(BigInteger number) {
+        numbersList.add(number);
+    }
+    
+    public BigInteger getNumber() {
+        return numbersList.get(cursor);
+    }
+    
+    /*
+     * Working on cursor
+     */
+
+    public void cursorForward() {
+        if(cursor < numbersList.size()-1) {
+            cursor++;
+        }
+    }
+    
+    public void cursorBackward() {
+        if(cursor > 0) {
+            cursor--;
+        }
+    }
+    
+    public void resetCursor() {
+        cursor = 0;
+    }
+    
+    /*
+     * Getters and setters
+     */
+    
+    public ArrayList<BigInteger> getNumbersList() {
+        return numbersList;
+    }
+}
