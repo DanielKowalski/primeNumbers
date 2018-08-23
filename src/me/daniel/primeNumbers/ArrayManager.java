a765f6c13038ff53f4ee6f0b5af82e7802f4f942
Daniel Kowalski <danielk.biznes@gmail.com>
2018-08-23 23:57:51 +0200

    Add array manager

---
+++ src/me/daniel/primeNumbers/ArrayManager.java
@@ -0,0 +1,12 @@
+package me.daniel.primeNumbers;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class ArrayManager {
+    private ArrayList<BigInteger> numbersList;
+    
+    public ArrayManager() {
+        numbersList = new ArrayList<BigInteger>();
+    }
+}
