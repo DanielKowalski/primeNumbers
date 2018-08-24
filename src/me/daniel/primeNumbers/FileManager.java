d306a426c051e87c51d26f345a032bcc77d808a0
Daniel Kowalski <danielk.biznes@gmail.com>
2018-08-24 15:37:56 +0200

    Add method to get list from file

--- src/me/daniel/primeNumbers/FileManager.java
+++ src/me/daniel/primeNumbers/FileManager.java
@@ -1,5 +1,6 @@
 package me.daniel.primeNumbers;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
@@ -7,12 +8,27 @@ import java.io.PrintWriter;
 import java.math.BigInteger;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class FileManager {
     private String fileName;
     
     FileManager(String fileName) {
         this.fileName = fileName;
+    }
+    
+    ArrayList<BigInteger> getListFromFile() {
+        ArrayList<BigInteger> numbersList = new ArrayList<BigInteger>();
+        try {
+            Scanner scan = new Scanner(new File(fileName));
+            while(scan.hasNextBigInteger()) {
+                numbersList.add(scan.nextBigInteger());
+            }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return numbersList;
     }
     
     void saveListToFile(ArrayList<BigInteger> list) {
